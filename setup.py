aac664ec2289e4457e85cbc449a3d2d9e1481e17
Thomas Samuel Binns <t.s.binns@outlook.com>
2023-05-12 13:41:36 +0200

    update versioning

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@ from setuptools import setup
 setup(
     name="PyPARRM",
     version="1.0.0",
-    license="MIT",
-    author="Thomas Samuel Binns",
-    author_email="t.s.binns@outlook.com",
     package_dir={"": "src/"},
     packages=["pyparrm", "pyparrm._utils"],
-    url="https://github.com/neuromodulation/PyPARRM",
-    install_requires=["numpy", "scipy", "matplotlib", "pqdm"],
 )
