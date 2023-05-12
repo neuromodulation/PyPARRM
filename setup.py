e98bf9c37e9d8945073eeb206fe7277212688bce
Thomas Samuel Binns <t.s.binns@outlook.com>
2023-05-12 13:43:10 +0200

    update versioning

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from setuptools import setup
 setup(
     name="PyPARRM",
     version="1.0.0",
-    package_dir={"": "src/"},
+    package_dir={"": "src"},
     packages=["pyparrm", "pyparrm._utils"],
 )
